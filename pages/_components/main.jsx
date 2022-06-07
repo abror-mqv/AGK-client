46bfd8ced0abc36895b4e0d2b8201e840b2118c6
Abror <mmtklvabrr@gmail.com>
2022-06-07 18:21:56 +0600

    ffgdf

---
+++ pages/_components/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './header/Index';
+import Navigation from './navigation';
+const Main = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Main;
