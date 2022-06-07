4e8c15ee3312c55e496cc237ad8bbe6e2dee8cc4
Abror <mmtklvabrr@gmail.com>
2022-06-07 18:22:01 +0600

    dsddfsd

--- pages/_components/blcoks/Blocks.jsx
+++ pages/_components/blcoks/Blocks.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import nasway from './Blocks.module.scss'
+
 import HomeBlock from './Home/Home';
 import LandingBlock from './Landing/LandingBlock';
 import Money from './Money/Money';
@@ -10,7 +12,7 @@ import Support from './Support/Support';
 
 const Blocks = () => {
     return (
-        <div>
+        <div className={nasway.Blocks}>
             <HomeBlock/>
             <LandingBlock/>
             <Stats/>
