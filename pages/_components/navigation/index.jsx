78f3fdcfff4b4ce4cf81e5f6b5ffae93073f699b
Abror <mmtklvabrr@gmail.com>
2022-06-10 20:00:53 +0600

    vercel deploy

--- pages/_components/navigation/index.jsx
+++ pages/_components/navigation/index.jsx
@@ -1,28 +1,40 @@
-import React from 'react';
-import { Stack } from '@mui/material';
-import NavButton from './components/NavButton/Index';
-import nasway from './Navigation.module.scss';
+import React from "react";
+import { Stack } from "@mui/material";
+import nasway from "./Navigation.module.scss";
+import Button from "@mui/material/Button";
+import Image from "next/image";
 
-
-// Importing Logos from assets 
-import contact from '../../assets/contacts.png'
-import portfolio from '../../assets/portfolio.png'
-import homepage from '../../assets/homepage.png'
-import news from '../../assets/news.png'
-import cabinett from '../../assets/cabinett.png'
-
+// Importing Logos from assets
+import contact from "../../assets/contacts.png";
+import portfolio from "../../assets/portfolio.png";
+import homepage from "../../assets/homepage.png";
+import news from "../../assets/news.png";
+import cabinett from "../../assets/cabinett.png";
 
 // Creating navbar using NavButton components inside of mui "Stack" component, giving them imported Logos as props
 function Navigation(props) {
-    return (
-        <Stack direction="row" spacing={5} className={nasway.NavigationStyle}>
-            <NavButton content={contact}/>
-            <NavButton content={portfolio}/>
-            <NavButton content={homepage}/>
-            <NavButton content={news}/>
-            <NavButton content={cabinett}/>
-        </Stack>
-    );
+  const NavButton = () => {
+    return <Button className={nasway.ButtonStyle}></Button>;
+  };
+  return (
+    <Stack direction="row" spacing={5} className={nasway.NavigationStyle}>
+      <NavButton className={nasway.ButtonStyle}>
+        <Image src={contact.src} alt="Contacts" />
+      </NavButton>
+      <NavButton className={nasway.ButtonStyle}>
+        <Image src={portfolio.src} alt="Portfolio" />
+      </NavButton>
+      <NavButton className={nasway.ButtonStyle}>
+        <Image src={homepage.src} alt="Home" />
+      </NavButton>
+      <NavButton className={nasway.ButtonStyle}>
+        <Image src={news.src} alt="News" />
+      </NavButton>
+      <NavButton className={nasway.ButtonStyle}>
+        <Image src={cabinett.src} alt="Cabinette" />
+      </NavButton>
+    </Stack>
+  );
 }
 
 export default Navigation;
